2c8052a9a8c52efe5c9299a606e742051670f123
Ben <bennchigozie@gmail.com>
2023-02-16 15:53:10 +0100

    Initialized Game Variables

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-(function startNewGame(){
-    (function displayGameOptions(){
+    (function (){
     let startButton = document.getElementById("start");
 
     startButton.addEventListener("click", function(){
@@ -13,6 +12,22 @@
     gameOptions.style.visibility = "hidden";
     title.style.visibility = "hidden";
     })
+    })();
+    (function (){
+        let gameForm = document.getElementById("game-form");
+        gameForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            
+            let gameForm = document.getElementById("game-form");
+            let formData = new FormData(gameForm);
+                
+            let data = Object.fromEntries(formData);
+
+            loadGame(data);
+
+        })
+
     })();
     (function hideGameOptions(){
         let beginButton = document.getElementById("begin");
@@ -29,4 +44,21 @@
         title.style.visibility = "visible";
         })
         })();
-})();
+    
+    
+    function loadGame(data){
+        
+        data.mode = +data.mode;
+        data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        data.playerOne = "Y";
+        data.playerTwo = "O";
+        data.round = 0;
+        data.currentPlayer = "X";
+        data.gameOver = false;
+    
+        console.log(data);
+    };
+
+
+
+
