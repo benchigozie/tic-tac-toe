82b68ccb88988bab27fa7976b99e5165254147d3
Ben <bennchigozie@gmail.com>
2023-02-23 16:16:13 +0100

    Set Win conditions and End conditoins

--- main.js
+++ main.js
@@ -13,6 +13,7 @@
     title.style.visibility = "hidden";
     })
     })();
+
     (function (){
         let gameForm = document.getElementById("game-form");
         gameForm.addEventListener("submit", (event) => {
@@ -29,6 +30,7 @@
         })
 
     })();
+
     (function hideGameOptions(){
         let beginButton = document.getElementById("begin");
     
@@ -48,6 +50,13 @@
     
     function loadGame(data){
         
+        initializeVariables(data);
+    
+        addEventListenersToGameBoard(data);
+    };
+
+    function initializeVariables(data){
+
         data.mode = +data.mode;
         data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
         data.playerOne = "Y";
@@ -55,9 +64,73 @@
         data.round = 0;
         data.currentPlayer = "X";
         data.gameOver = false;
-    
-        console.log(data);
+
     };
+
+    function addEventListenersToGameBoard(data){
+
+        document.querySelectorAll(".board-square").forEach(box => {
+            
+            box.addEventListener("click", (event) => {
+                playMove(event.target, data);
+            });
+
+        });
+    };
+
+    const winConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    function playMove(square, data){
+
+        if (data.gameOver === true) {return};
+        if ((data.board[square.id] === "X") || (data.board[square.id] === "O")){return};
+
+        data.board[square.id] = data.currentPlayer;
+        square.textContent = data.currentPlayer;
+        square.classList.add(data.currentPlayer === "X" ? "player1" : "player2");
+
+        endConditions(data);
+
+    };
+
+    function checkWinner(data){ 
+        
+        let result = false;
+
+        winConditions.forEach((condition) => {
+            if (data.board[condition[0]] === data.board[condition[1]] && data.board[condition[1]] === data.board[condition[2]]){
+                result = true;
+            }
+        });
+
+        return result;
+
+    };
+
+    function endConditions(data){
+
+        console.log(checkWinner(data));
+
+        if (checkWinner(data)){
+            return true;
+        }
+        else if (data.round === 9){
+            return true;
+        }
+        return false;
+        
+    };
+
+
 
 
 
