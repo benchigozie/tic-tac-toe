04a591b28a5ae9d449f47bbee8555b779338a531
Ben <bennchigozie@gmail.com>
2023-02-26 21:42:56 +0100

    Completed multiplayer Logic

--- main.js
+++ main.js
@@ -6,12 +6,18 @@
     let gameSection = document.getElementById("game-section");
     let gameOptions = document.getElementById("game-options");
     let title = document.querySelector("header");
+    let infoBox = document.getElementById("info-display");
 
     gamePlayInfo.style.display = "flex";
     gameSection.style.visibility = "hidden";
     gameOptions.style.visibility = "hidden";
     title.style.visibility = "hidden";
+    infoBox.style.visibility = "hidden";
+
     })
+        
+    startButton.classList.add("restart");
+
     })();
 
     (function (){
@@ -24,6 +30,8 @@
             let formData = new FormData(gameForm);
                 
             let data = Object.fromEntries(formData);
+
+            assignNames(data);
 
             loadGame(data);
 
@@ -39,31 +47,54 @@
         let gameSection = document.getElementById("game-section");
         let gameOptions = document.getElementById("game-options");
         let title = document.querySelector("header");
+        let infoBox = document.getElementById("info-display");
     
         gamePlayInfo.style.display = "none";
         gameSection.style.visibility = "visible";
         gameOptions.style.visibility = "visible";
         title.style.visibility = "visible";
+        infoBox.style.visibility = "visible";
+
+        document.getElementById("start").textContent = "Restart Game";
+
         })
-        })();
-    
-    
+
+    })();
+
     function loadGame(data){
+
+        (function (){
+
+            let restartButton = document.querySelector(".restart");
+
+            restartButton.addEventListener("click", () => {
+                
+                resetGame(data);
+                initializeVariables(data);
+
+            });
+
+        })();
         
         initializeVariables(data);
     
         addEventListenersToGameBoard(data);
+
     };
 
     function initializeVariables(data){
 
         data.mode = +data.mode;
         data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        data.playerOne = "Y";
+        data.playerOne = "X";
         data.playerTwo = "O";
         data.round = 0;
         data.currentPlayer = "X";
         data.gameOver = false;
+        data.winner = "";
+        data.playing = data["player-one-name"];
+        data.pOneBackup = data["player-one-name"];
+        data.pTwoBackup = data["player-one-name"];
 
     };
 
@@ -72,10 +103,14 @@
         document.querySelectorAll(".board-square").forEach(box => {
             
             box.addEventListener("click", (event) => {
+
                 playMove(event.target, data);
             });
 
         });
+
+        playersTurn(data);
+    
     };
 
     const winConditions = [
@@ -95,10 +130,12 @@
         if ((data.board[square.id] === "X") || (data.board[square.id] === "O")){return};
 
         data.board[square.id] = data.currentPlayer;
+        data.round += 1;
         square.textContent = data.currentPlayer;
         square.classList.add(data.currentPlayer === "X" ? "player1" : "player2");
 
-        endConditions(data);
+        displayTurn(data);
+        resetGameVars(data);
 
     };
 
@@ -108,6 +145,15 @@
 
         winConditions.forEach((condition) => {
             if (data.board[condition[0]] === data.board[condition[1]] && data.board[condition[1]] === data.board[condition[2]]){
+                
+                data.winner = data.playing;
+
+                (function (){
+                document.querySelectorAll(".board-square")[condition[0]].style.background = "rgba(48, 126, 150, 0.8)";
+                document.querySelectorAll(".board-square")[condition[1]].style.background = "rgba(48, 126, 150, 0.8)";
+                document.querySelectorAll(".board-square")[condition[2]].style.background = "rgba(48, 126, 150, 0.8)";
+                })();
+
                 result = true;
             }
         });
@@ -118,9 +164,8 @@
 
     function endConditions(data){
 
-        console.log(checkWinner(data));
-
         if (checkWinner(data)){
+
             return true;
         }
         else if (data.round === 9){
@@ -130,8 +175,95 @@
         
     };
 
+    function endGame(data){
+        
+        announceGameVerdict(data);
+        
+    };
+
+    function gameMode(){
+        
+    };
+
+    function announceGameVerdict (data){
+
+        if(checkWinner(data)){
+            data.infoBox.textContent = `${data.playing} has won the game`;
+        }
+        else {
+            data.infoBox.textContent = `It's a Tie`; 
+        }
+
+    };
+
+    function resetGame (data){
+        let ide = 0;
+        document.querySelectorAll(".board-square").forEach(box => {
+            
+            box.textContent = "";
+            box.style.background = "rgba(86, 95, 95, 0.671)";
+            
+            box.id = ide;
+
+            if (ide <= 8){
+            ide = ide + 1;
+
+        }
+
+        }); 
+
+    };
+
+    function playersTurn (data){
+
+        data.infoBox = document.getElementById("info-box");
+        data.infoBox.textContent = `${data.playing}'s Turn (${data.currentPlayer})`;
+
+    };
+    function resetGameVars(data){
+        let resetButton = document.getElementById("reset");
+
+        console.log(data["player-two-name"]);
+        resetButton.addEventListener("click", function (){
+
+            assignNames(data);
+            loadGame(data);
+            displayTurn(data);
+            resetGame(data);
+            displayTurn(data);
+
+        })
+
+    };
+    function assignNames(data){
+        if (data["player-one-name"] === ""){
+            data["player-one-name"] = "player 1";
+        }
+        if(data["player-two-name"] === ""){
+            data["player-two-name"] = "player 2";
+        }    
+    }
+    function displayTurn(data){
+        if(endConditions(data)){
+
+            data.gameOver = true;
+            endGame(data);
+
+        }
+        else if(data.currentPlayer === "X"){
+            data.currentPlayer = "O";
+            data.playing = data["player-two-name"];
+            playersTurn(data);
+        }
+        else {
+            data.currentPlayer = "X";
+            data.playing = data["player-one-name"];
+            playersTurn(data);
+        }
+    };
 
 
 
 
 
+ 
